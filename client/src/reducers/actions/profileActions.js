8abc4088e2da64c437cfeb6074e9e1c62bfcdbc0
Kevin <khom415@gmail.com>
2020-10-14 21:38:46 -0700

    Added get request for fetching profile information

--- client/src/reducers/actions/profileActions.js
+++ client/src/reducers/actions/profileActions.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 import {
-    LOGOUT_PROFILE, FETCH_ORDER_HISTORY
+    LOGOUT_PROFILE, FETCH_ORDER_HISTORY, FETCH_PROFILE
 } from './profileTypes';
 
 import { API_URL } from '../constants'
@@ -25,6 +25,29 @@ export const fetchOrderHistory = (customer_uid) => dispatch => {
             console.log(res.data.result);
             dispatch({
                 type: FETCH_ORDER_HISTORY,
+                payload: res.data.result,
+            })
+        })
+        .catch((err) => {
+            console.log(err);
+        })
+
+}
+
+export const fetchProfile = () => dispatch => {
+    axios
+        .get(API_URL + 'Profile/100-000082')
+        /*
+        .get(API_URL + 'Profile',{
+            params: {
+                customer_uid: customer_uid,
+            }
+        })
+        */
+        .then((res) => {
+            console.log(res.data.result);
+            dispatch({
+                type: FETCH_PROFILE,
                 payload: res.data.result,
             })
         })
